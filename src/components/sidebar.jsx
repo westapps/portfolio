d299290a61ac46370c788d8b109e84b61cfb046f
Simon <greenspanx@hotmail.com>
2019-08-13 15:17:50 +0800

    update

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -25,7 +25,7 @@ export default class Sidebar extends Component {
                   <li><a href="#about" data-nav-section="about">Technical Skills</a></li>
                   <li><a href="#timeline" data-nav-section="timeline">Qualification & Certificates</a></li>
                   <li><a href="#spareLearning" data-nav-section="spareLearning">Spare Time Learning</a></li>
-                  <li><a href="#experience" data-nav-section="experience">Relevant Professional Experience</a></li>
+                  <li><a href="#projects" data-nav-section="projects">Hobby Projects</a></li>
                 </ul>
               </div>
             </nav>
