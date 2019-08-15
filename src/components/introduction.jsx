c2653fcb03c9b8e7c0804ca8e773e19a40d44556
Simon <greenspanx@hotmail.com>
2019-08-15 12:28:11 +0800

    skill assessment

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -8,11 +8,11 @@ export default class Introduction extends Component {
         <section id="colorlib-hero" className="js-fullheight" data-section="home">
           <div className="flexslider js-fullheight">
             <div className="hero flex-center">
-				<div className="hero-message">
-					<h1 className="hero-title">pushing the boundaries of what's possible</h1>
-					<h2 className="hero-sub-title">Full Stack Developer That Can Help</h2>
-				</div>
-			</div>
+      				<div className="hero-message">
+      					<h1 className="hero-title">pushing the boundaries of what's possible</h1>
+      					<h2 className="hero-sub-title">Full Stack Developer That Can Help</h2>
+      				</div>
+      			</div>
           </div>
         </section>
       </div>
--- src/components/skills_assessment.jsx
+++ src/components/skills_assessment.jsx
@@ -9,18 +9,26 @@ export default class Assessment extends Component {
           <h2 className="colorlib-heading">Skills Assessments on Pluralsight.com</h2>
             <div className="row">
               <div className="col-md-6 gallery_gap">
-                <img src="images/javascript_assessment.jpg" className="gallery" alt="Demo"></img>
+                <img src="images/javascript_assessment.jpg" className="gallery" alt="javascript_assessment"></img>
               </div>
               <div className="col-md-6 gallery_gap">
-                <img src="images/nodejs.assessment.jpg" className="gallery" alt="Demo"></img>
+                <img src="images/nodejs_assessment.jpg" className="gallery" alt="nodejs_assessment"></img>
               </div>
             </div>
             <div className="row">
               <div className="col-md-6 gallery_gap">
-                <img src="images/html5_assessment.jpg" className="gallery" alt="Demo"></img>
+                <img src="images/html5_assessment.jpg" className="gallery" alt="html5_assessment"></img>
               </div>
               <div className="col-md-6 gallery_gap">
-                <img src="images/react_assessment.jpg" className="gallery" alt="Demo"></img>
+                <img src="images/react_assessment.jpg" className="gallery" alt="react_assessment"></img>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6 gallery_gap">
+                <img src="images/css_assessment.jpg" className="gallery" alt="css_assessment"></img>
+              </div>
+              <div className="col-md-6 gallery_gap">
+
               </div>
             </div>
           </div>
